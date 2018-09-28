794886ce89711962c0f844624ad0cba783486a1d
apalion1493 <39518411+apalion1493@users.noreply.github.com>
2018-09-28 12:03:18 +0300

    first commit

---
+++ src/assets/js/main.js
@@ -0,0 +1,16 @@
+//= ../../../bower_components/jquery/dist/jquery.js
+
+//= ../../../bower_components/popper.js/dist/umd/popper.js
+//= ../../../bower_components/bootstrap/js/dist/util.js
+//= ../../../bower_components/bootstrap/js/dist/tooltip.js
+//= ../../../bower_components/bootstrap/js/dist/popover.js
+//= ../../../bower_components/bootstrap/js/dist/carousel.js
+//= ../../../bower_components/bootstrap/js/dist/tab.js
+//= ../../../bower_components/bootstrap/js/dist/modal.js
+
+
+//= customs/easings.js
+//= customs/common.js
+//= customs/fullpage.js
+
+
